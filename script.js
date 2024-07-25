c57fe66bfbed2f3333de5ca4d4c92aaeed24fa65
OmkarShipurkar1 <omkarshipurkar39@gmail.com>
2024-07-25 17:08:42 +0530

    completed successfully

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
+// initial
 let humanScore = 0;
 let computerScore = 0;
-const choices = ["rock", "scissor", "paper"];
 let randomNumber = Math.trunc(Math.random() * 3);
+const choices = ["rock", "scissors", "paper"];
+const result = document.querySelector(".result");
+
+const rockBtn = document.querySelector(".rock");
+const scissorsBtn = document.querySelector(".scissors");
+const paperBtn = document.querySelector(".paper");
+
+const p = document.createElement("p");
+const cp = document.createElement("p");
+const hp = document.createElement("p");
+
+const scores = document.querySelector(".scores");
+const userScore = document.querySelector(".user-score");
+const compScore = document.querySelector(".comp-score");
 
 // Computer
 const getComputerChoice = function () {
@@ -16,41 +30,76 @@ const getHumanChoice = function () {
   return userChoice;
 };
 
+cp.classList.add("font-cp");
+hp.classList.add("font-hp");
+
+let playing = true;
+
 const playRound = function (humanChoice, computerChoice) {
-  if (humanChoice === computerChoice) console.log("It's a Tie! Try Again.");
-  else if (
+  result.classList.add("bg");
+  p.style.fontSize = "3rem";
+  scores.classList.add("styles");
+  p.style.textAlign = "center";
+
+  cp.textContent = `Computer Choose : ${computerChoice}`;
+  hp.textContent = `You Choose : ${humanChoice}`;
+
+  result.appendChild(hp);
+  result.appendChild(cp);
+
+  if (humanChoice === computerChoice) {
+    p.textContent = "It's a Tie! Try Again.";
+  } else if (
     (humanChoice === "rock" && computerChoice === "scissors") ||
     (humanChoice === "scissors" && computerChoice === "paper") ||
     (humanChoice === "paper" && computerChoice === "rock")
   ) {
-    console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
+    p.textContent = `You Win! ${humanChoice} beats ${computerChoice}`;
+
     humanScore++;
   } else {
-    console.log(`You Lose! ${computerChoice} beats ${humanChoice}`);
+    p.textContent = `You Lose! ${computerChoice} beats ${humanChoice}`;
     computerScore++;
   }
-};
 
-let computerSelection = getComputerChoice();
+  userScore.textContent = `YOUR SCORE : ${humanScore}`;
+  userScore.style.textAlign = "left";
 
-const playGame = function () {
-  for (let i = 0; i < 5; i++) {
-    let humanSelection = getHumanChoice();
-    console.log("You : " + humanSelection);
-    randomNumber = Math.trunc(Math.random() * 3);
-    computerSelection = getComputerChoice();
-    console.log("Computer : " + computerSelection);
-    playRound(humanSelection, computerSelection);
+  compScore.textContent = `COMP SCORE : ${computerScore}`;
+  compScore.style.textAlign = "right";
 
-    console.log(
-      `Current Score: \nYou : ${humanScore}\nComputer : ${computerScore}`
-    );
+  if (humanScore === 5) {
+    scores.classList.remove("styles");
+    playing = false;
+    userScore.textContent = "You Win!";
+    userScore.style.textAlign = "center";
+    compScore.remove();
   }
 
-  if (humanScore === computerScore) return "It's a tie";
-  return humanScore > computerScore
-    ? `You Win! (${humanScore} > ${computerScore}) `
-    : `Computer Win! (${computerScore} > ${humanScore}) `;
+  if (computerScore === 5) {
+    scores.classList.remove("styles");
+    playing = false;
+    compScore.textContent = "Computer Win!";
+    compScore.style.textAlign = "center";
+    userScore.remove();
+  }
+  result.appendChild(p);
 };
 
-console.log(playGame());
+rockBtn.addEventListener("click", function () {
+  randomNumber = Math.trunc(Math.random() * 3);
+  const computerSelection = getComputerChoice();
+  if (playing === true) playRound("rock", computerSelection);
+});
+
+scissorsBtn.addEventListener("click", function () {
+  randomNumber = Math.trunc(Math.random() * 3);
+  const computerSelection = getComputerChoice();
+  if (playing === true) playRound("scissors", computerSelection);
+});
+
+paperBtn.addEventListener("click", function () {
+  randomNumber = Math.trunc(Math.random() * 3);
+  const computerSelection = getComputerChoice();
+  if (playing === true) playRound("paper", computerSelection);
+});
